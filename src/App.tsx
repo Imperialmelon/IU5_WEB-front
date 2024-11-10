aebf60c8010d295dd00d73763b620458e1360d4f
Imperialmelon <bogart30@yandex.ru>
2024-11-10 17:22:07 +0300

    tauri application

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import {  BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./Routes";
 import {Provider} from "react-redux";
 import {store} from "./core/store";
+import {useEffect} from "react";
 // import { useGlobalProps } from "./hooks/useGlobalProps";
 
-function App() {
-  // const globalProps = useGlobalProps();
-  return (
-      <BrowserRouter basename="/IU5_WEB-front">
-        <Provider store={store}>
-        <AppRoutes />
-        </Provider>
 
-      </BrowserRouter>
-  );
+
+const {invoke} = (window as any).__TAURI__.tauri;
+
+
+function App() {
+    useEffect(() => {
+        invoke('tauri', {cmd: 'create'})
+            .then((response: any) => console.log(response))
+            .catch((error: any) => console.log(error));
+        return () => {
+            invoke('tauri', {cmd: 'close'})
+                .then((response: any) => console.log(response))
+                .catch((error: any) => console.log(error));
+        }
+    }, []);
+
+    return (
+        <BrowserRouter basename="/IU5_WEB-front">
+            <Provider store={store}>
+                <AppRoutes  />
+            </Provider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
