421e16afeb223cb0d70d593f1582b87fee182c77
Imperialmelon <bogart30@yandex.ru>
2024-11-21 12:55:09 +0300

    working build

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,6 @@
-#[cfg(debug_assertions)]
-use tauri::Manager;
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri::Builder::default()
-        .setup(|app| {
-            #[cfg(debug_assertions)]
-            {
-                let window = app.get_window("main").unwrap();
-                window.open_devtools();
-            }
-            Ok(())
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  app_lib::run();
 }
