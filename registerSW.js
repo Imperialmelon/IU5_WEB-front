9354bd5c654d8309143faea0b1a88d59eea201e8
Imperialmelon <bogart30@yandex.ru>
2024-12-15 15:54:30 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/IU5_WEB-front/sw.js', { scope: '/IU5_WEB-front/' })})}
